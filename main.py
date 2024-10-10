d145181d92b6d771edaa8c694525f05cc0b8cf68
seblessa <sebastiaolessa2002@outlook.com>
2024-10-10 19:31:56 +0100

    update

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import ir_datasets
 
-dataset = ir_datasets.load("nfcorpus")
-for doc in dataset.docs_iter():
-    doc
+dataset = ir_datasets.load("beir/nfcorpus")
+
+docs = dataset.docs_iter()
+
+queries = dataset.queries_iter()
+
+
+# Create a pandas df with a col for the id, title, text, and url
+
+import pandas as pd
+
+df = pd.DataFrame(columns=['id', 'title', 'text', 'url'])
+for iter in docs:
+    df = df.append({'id': iter.doc_id, 'title': iter.title, 'text': iter.text, 'url': iter.url}, ignore_index=True)
 
 
